9d982f188201dbefe4ac04b43a15c7380f246856
Ritik <Ritikranaagra82@gmail.com>
2025-12-05 09:00:00 +0000

    Add Java examples Dec2025 (4/12)

---
+++ Java/Queue_03.java
@@ -0,0 +1,20 @@
+// Queue using Linked List and Array Deque (Java Collections Framework)
+
+import java.util.*;
+
+public class Queue_03 {
+    public static void main(String args[]) {
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
