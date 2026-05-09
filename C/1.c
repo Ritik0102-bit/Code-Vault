9e470edd9125dca1e403a2b2d11bfbb1f2977dc5
Ritik Rana <Ritikranaagra82@gmail.com>
2026-05-09 23:41:21 +0530

    Basics of C

---
+++ C/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int age=22;
+    float pi=3.14;
+    char name='*';
+    printf("Hello World \n");
+    printf("Hello World \n");
+    printf("Hello World \n");
+    printf("Hello World \n");
+    printf("age is %d \n",age);
+    printf("value of pi is %f \n",pi);
+    printf("my name is %c \n",name);
+    return 0;
+}
