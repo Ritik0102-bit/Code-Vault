60c8dfbbaea8dff1eaa69c48e41bd313c1ad844d
Ritik Rana <Ritikranaagra82@gmail.com>
2026-05-11 21:55:24 +0530

    Graph Implementation Using ArrayList [Adjacency List]

---
+++ Java/Graph_00.java
@@ -0,0 +1,48 @@
+// Graph Implementation Using ArrayList [Adjacency List]
+
+import java.util.ArrayList;
+
+public class Graph_00 {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void Create_Graph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        Create_Graph(graph);
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                System.out.println(e.src + " -> " + e.dest);
+            }
+        }
+    }
+}
