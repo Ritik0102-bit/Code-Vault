a42a6528a788282a0f1def8e4381c3eee3e7bc19
Ritik Rana <Ritikranaagra82@gmail.com>
2026-06-28 00:58:27 +0530

    Two's Complement

---
+++ C++/Two's_Complement.cpp
@@ -0,0 +1,18 @@
+// Suppose n=-10
+
+// step 1 :- convert 10 to its binary form 
+// 1010
+
+// step 2 :- add a prefix 0
+// 01010
+
+// step 3 :- Now find its 1's Complement
+// 10101
+
+// step 4 :- Now add 1 to it
+// 10101
+//  +  1
+// ------
+// 10110 
+
+// so the two's complement of -10 is 10110
